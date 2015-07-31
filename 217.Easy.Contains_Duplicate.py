739ade430230174d4e8fa74ee7debbb59c6837ae
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-07-31 20:06:49 +0800

    Time Limit Exceeded

--- 217.Easy.Contains_Duplicate.py
+++ 217.Easy.Contains_Duplicate.py
@@ -2,4 +2,9 @@ class Solution:
     # @param {integer[]} nums
     # @return {boolean}
     def containsDuplicate(self, nums):
+    	# # Time Limit Exceeded
+    	# for i, n in enumerate(nums):
+     #        if i != nums.index(n):
+     #            return True
+     #    return False
         return len(set(nums)) < len(nums)
