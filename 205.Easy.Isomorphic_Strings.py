f8445f8d06842ea97a49dcb12bc71735a167b730
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-03 12:27:44 +0800

    205.Easy.Isomorphic_Strings.py
    str.index()

---
+++ 205.Easy.Isomorphic_Strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param {string} s
+    # @param {string} t
+    # @return {boolean}
+    def isIsomorphic(self, s, t):
+        if len(s) == len(t):
+            for i in xrange(len(s)):
+                if s.index(s[i]) != t.index(t[i]):
+                    return False
+            return True
+        return False
