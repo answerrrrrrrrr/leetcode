d1ba0f85a4bea5c17b1d9b297a4ab0689fe1cc3c
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-03 13:43:39 +0800

    zip & set
    https://leetcode.com/discuss/48674/python-different-solutions-dictionary
    -etc

--- 205.Easy.Isomorphic_Strings.py
+++ 205.Easy.Isomorphic_Strings.py
@@ -3,9 +3,13 @@ class Solution:
     # @param {string} t
     # @return {boolean}
     def isIsomorphic(self, s, t):
-        if len(s) == len(t):
-            for i in xrange(len(s)):
-                if s.index(s[i]) != t.index(t[i]):
-                    return False
-            return True
-        return False
+        # if len(s) == len(t):
+        #     for i in xrange(len(s)):
+        #         if s.index(s[i]) != t.index(t[i]):
+        #             return False
+        #     return True
+        # return False
+
+
+
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
