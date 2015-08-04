2d1d00191431bd0c240fdd68e434b46448e1ae8c
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-05 07:18:33 +0800

    203*.Easy.Remove_Linked_List_Element.py
    dummy head

---
+++ 203*.Easy.Remove_Linked_List_Element.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} val
+    # @return {ListNode}
+    def removeElements(self, head, val):
+        dummy = ListNode(-1)
+        dummy.next = head
+        p = dummy
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p= p.next
+        return dummy.next
