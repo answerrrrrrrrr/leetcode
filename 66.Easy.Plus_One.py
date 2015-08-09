e4303674d0db42832629d7046308c9d0df8a111f
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-09 09:52:30 +0800

    66.Easy.Plus_One.py
    #divmod
    for…else

---
+++ 66.Easy.Plus_One.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param {integer[]} digits
+    # @param {integer[]}
+    def plusOne(self, digits):
+        # carry = 1
+        # for i in range(len(digits)-1, -1, -1):
+        #     carry, digits = divmod(digits[i]+carry, 10)
+        #     if carry == 0:
+        #         break
+        # if carry:
+        #     digits.insert(0, carry)
+        # return digits
+        for i in range(len(digits)-1, -1, -1):
+            digits[i] = (digits+1) % 10
+            if digits[i]:
+                break
+        else:
+            digits.insert(0,1)
+        return digits
