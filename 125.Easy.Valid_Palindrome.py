635adc97fce7ed6fb1ceabec93c8bdacdeca1a08
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-12 09:25:46 +0800

    125.Easy.Valid_Palindrome.py

--- 125.Easy.Valid_Palindrome.py
+++ 125.Easy.Valid_Palindrome.py
@@ -1,6 +1,17 @@
 class Solution:
-    # @param {string} s
-    # @return {boolean}
-    def isPalindrome:
-        s = filter(str.isalnum(), s.lower())
+    # @param s, a string
+    # @return a boolean
+    def isPalindrome(self, s):
+        # import re
+        # a = re.sub(r"\W+", r"", s.lower())
+        # return a == a[::-1]
+
+
+
+        # s = "".join([c.lower() for c in s if c.isalnum()])
+        # return s == s[::-1]
+        
+        
+        
+        s = filter(lambda x: x.isalnum(), s.lower())
         return s == s[::-1]
