bd8aaf94f74baea0a587da12375a649d463b03f7
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-13 17:42:40 +0800

    21.Easy.Merge_Two_Sorted_List.py
    return a or b
    —— if a: return a; else: return b
    
    Similarly，c = a or b …

---
+++ 21.Easy.Merge_Two_Sorted_List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} l1
+    # @param {ListNode} l2
+    # @return {ListNode}
+    def mergeTwoLists(self, l1, l2):
+        p = dummy = ListNode(-1)
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = l1 
+                l1 = l1.next
+            else:
+                p.next = l2 
+                l2 = l2.next
+            p = p.next
+        p.next = l1 or l2 
+        return dummy.next
