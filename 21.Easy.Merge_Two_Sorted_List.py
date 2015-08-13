1f5f067887c7112fdf374e50b366394dc73528e8
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-13 17:57:11 +0800

    21.Easy.Merge_Two_Sorted_List.py
    if l1.val > l2.val:
                    l1, l2 = l2, l1

--- 21.Easy.Merge_Two_Sorted_List.py
+++ 21.Easy.Merge_Two_Sorted_List.py
@@ -9,14 +9,23 @@ class Solution:
     # @param {ListNode} l2
     # @return {ListNode}
     def mergeTwoLists(self, l1, l2):
-        p = dummy = ListNode(-1)
-        while l1 and l2:
-            if l1.val < l2.val:
-                p.next = l1 
-                l1 = l1.next
-            else:
-                p.next = l2 
-                l2 = l2.next
-            p = p.next
-        p.next = l1 or l2 
-        return dummy.next
+        # p = dummy = ListNode(-1)
+        # while l1 and l2:
+        #     if l1.val < l2.val:
+        #         p.next = l1 
+        #         l1 = l1.next
+        #     else:
+        #         p.next = l2 
+        #         l2 = l2.next
+        #     p = p.next
+        # p.next = l1 or l2 
+        # return dummy.next
+
+
+
+        # Recursive Solution
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1 
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
