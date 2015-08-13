15f4eeee3d571d4f7614f06fab33c4f44b1e7c36
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-13 19:27:38 +0800

    83.Easy.Remove_Duplicates_from_Sorted_List.py
    So slow…

---
+++ 83.Easy.Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def deleteDuplicates(self, head):
+        p = head
+        while p and p.next:
+            if p.val == p.next.val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return head
