8c39750a371d9f003a7e3cb9e31592f33dafa906
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-14 09:59:35 +0800

    88.Easy.Merge_Sorted_Array.py

---
+++ 88.Easy.Merge_Sorted_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer} m
+    # @param {integer[]} nums2
+    # @param {integer} n
+    # @return {void} Do not return anything, modify nums1 in-place instead.
+    def merge(self, nums1, m, nums2, n):
+        if n:
+            nums1[:] = nums1[:-n]
+        nums1[:] = sorted(nums1 + nums2)
