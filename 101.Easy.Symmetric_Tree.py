ca6b5aabbffa112ca8f068eef581564061b1df51
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-16 10:28:24 +0800

    101.Easy.Symmetric_Tree.py
    l, r = queue.pop(0)
    iterable assignment

--- 101.Easy.Symmetric_Tree.py
+++ 101.Easy.Symmetric_Tree.py
@@ -8,12 +8,35 @@
 class Solution:
     # @param {TreeNode} root
     # @return {boolean}
+
+ # Recursive Solution   
+    # def isSymmetric(self, root):
+    #     if not root:
+    #         return True
+    #     return self.s(root.left, root.right)
+
+    # def s(self, l, r):
+    #     if l and r:
+    #         return l.val == r.val and self.s(l.left, r.right) and self(l.right, r.left)
+    #     return l == r
+
+
+
+# Iterative Solution
+# https://leetcode.com/discuss/26372/python-iterative-way-using-a-queue?show=44691#a44691
     def isSymmetric(self, root):
         if not root:
             return True
-        return self.s(root.left, root.right)
-
-    def s(self, l, r):
-        if l and r:
-            return l.val == r.val and self.s(l.left, r.right) and self(l.right, r.left)
-        return l == r
+        queue = []
+        queue.append((root.left, root.right))
+        while queue:
+            l, r = queue.pop(0)
+            if not l and not r:
+                continue
+            if not l or not r:
+                return False
+            if l.val != r.val:
+                return False
+            queue.append((l.left, r.right))
+            queue.append((l.right, r.left))
+        return True
