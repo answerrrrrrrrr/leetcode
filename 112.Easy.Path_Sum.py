42556754c85282d745168dc4f23575177530df34
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-17 09:08:46 +0800

    112.Easy.Path_Sum.py
    Iterative Solution(faster)

--- 112.Easy.Path_Sum.py
+++ 112.Easy.Path_Sum.py
@@ -10,9 +10,26 @@ class Solution:
     # @param {integer} sum
     # @return {boolean}
     def hasPathSum(self, root, sum):
+    # Recursive Solution
+    #     if not root:
+    #         return False
+    #     if sum == root.val and not root.left and not root.right:
+    #         return True
+    #     hps = self.hasPathSum
+    #     return hps(root.left, sum - root.val) or hps(root.right, sum - root.val)
+        
+
+
+    # Iterative Solution
         if not root:
             return False
-        if sum == root.val and not root.left and not root.right:
-            return True
-        hps = self.hasPathSum
-        return hps(root.left, sum - root.val) or hps(root.right, sum - root.val)
+        stack = [(root, sum)]
+        while stack:
+            node, sum = stack.pop()
+            if node.left is node.right is None and node.val == sum:
+                return True
+            if node.left:
+                stack.append((node.left, sum - node.val))
+            if node.right:
+                stack.append((node.right, sum - node.val))
+        return False
