e236055ccfabbe7843021a792fad35f2ba757cd5
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-18 14:22:01 +0800

    111.Easy.Minimum_Depth_of_Binary_Tree.py
    stack.pop(0)

--- 111.Easy.Minimum_Depth_of_Binary_Tree.py
+++ 111.Easy.Minimum_Depth_of_Binary_Tree.py
@@ -19,11 +19,20 @@ class Solution:
 
 
     # Recursive Solution with map()
-        if not root: return 0
-        d = map(self.minDepth, (root.left, root.right))
-        return 1 + (min(d) or max(d))
+        # if not root: return 0
+        # d = map(self.minDepth, (root.left, root.right))
+        # return 1 + (min(d) or max(d))
 
 
 
-
-    
+    # Iterative Solution
+        stack = [(root, 1)] if root else []
+        while stack:
+            node, depth = stack.pop(0)
+            if node.left is node.right is None:
+                return depth
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return 0
