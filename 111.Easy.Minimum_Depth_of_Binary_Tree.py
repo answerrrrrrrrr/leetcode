2546df1433e79f5fc60df742e2d238e1d7b485b3
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-18 14:20:22 +0800

    111.Easy.Minimum_Depth_of_Binary_Tree.py
    https://leetcode.com/discuss/41766/3-lines-in-every-language

--- 111.Easy.Minimum_Depth_of_Binary_Tree.py
+++ 111.Easy.Minimum_Depth_of_Binary_Tree.py
@@ -9,9 +9,21 @@ class Solution:
     # @param {TreeNode} root
     # @return {integer}
     def minDepth(self, root):
-        if not root:
-            return 0
-        md = self.minDepth
-        if not root.left or not root.right:
-            return md(root.left) + md(root.right) + 1
-        return min(md(root.left), md(root.right)) + 1
+    # Recursive Solution
+        # if not root:
+        #     return 0
+        # md = self.minDepth
+        # if not root.left or not root.right:
+        #     return md(root.left) + md(root.right) + 1
+        # return min(md(root.left), md(root.right)) + 1
+
+
+    # Recursive Solution with map()
+        if not root: return 0
+        d = map(self.minDepth, (root.left, root.right))
+        return 1 + (min(d) or max(d))
+
+
+
+
+    
