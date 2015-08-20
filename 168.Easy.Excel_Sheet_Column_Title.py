6464d003d61c7cf5c394d2ffb35290780bfdb194
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-20 10:16:08 +0800

    168.Easy.Excel_Sheet_Column_Title.py
    Recursive

--- 168.Easy.Excel_Sheet_Column_Title.py
+++ 168.Easy.Excel_Sheet_Column_Title.py
@@ -2,8 +2,13 @@ class Solution:
     # @param {integer} n
     # @return {string}
     def convertToTitle(self, n):
-        ret = ''
-        while n:
-            ret = chr((n - 1) % 26 + ord('A')) + ret
-            n = (n - 1) / 26
-        return ret
+        # ret = ''
+        # while n:
+        #     ret = chr((n - 1) % 26 + ord('A')) + ret
+        #     n = (n - 1) / 26
+        # return ret
+
+
+
+    # Recursive Solution
+        return '' if n == 0 else self.convertToTitle((n-1)/26) + chr((n-1)%26+65)
