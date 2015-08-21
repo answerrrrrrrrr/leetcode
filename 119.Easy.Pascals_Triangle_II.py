756224555f1383573ae1af0adab98ec513846e80
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-21 19:22:27 +0800

    119.Easy.Pascals_Triangle_II.py
    math

--- 119.Easy.Pascals_Triangle_II.py
+++ 119.Easy.Pascals_Triangle_II.py
@@ -12,8 +12,17 @@ class Solution(object):
         # return retRow
 
 
-        
-        retRow = [1]
-        for i in xrange(rowIndex):
-            retRow = map(lambda x, y: x+y, [0]+retRow, retRow+[0])
-        return retRow
+
+        # retRow = [1]
+        # for i in xrange(rowIndex):
+        #     retRow = map(lambda x, y: x+y, [0]+retRow, retRow+[0])
+        # return retRow
+
+
+    # https://leetcode.com/discuss/9475/fastest-%26-shortest-solution-in-python
+        r = [1]
+        for i in xrange(1,rowIndex+1):
+          r.append( r[-1]*(rowIndex-i+1)/i )
+        return r
+
+        # return reduce(lambda lst,i: lst + [lst[-1]*(rowIndex-i+1)/i], [[1]] + range(1,rowIndex+1))
