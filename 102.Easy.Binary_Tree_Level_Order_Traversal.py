9b5874277c06537607634a0b7b5b87bf3df9aa91
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-22 10:43:09 +0800

    102.Easy.Binary_Tree_Level_Order_Traversal.py

--- 102.Easy.Binary_Tree_Level_Order_Traversal.py
+++ 102.Easy.Binary_Tree_Level_Order_Traversal.py
@@ -11,7 +11,7 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-    # BFS
+    # BFS 3Lists
     # 60ms+
         # if not root:
         #     return []
@@ -32,15 +32,34 @@ class Solution(object):
 
     # DFS Recursively
     # 50ms+
-        res = []
-        self.dfs(root, 0, res)
+    #     res = []
+    #     self.dfs(root, 0, res)
+    #     return res
+
+    # def dfs(self, root, level, res):
+    #     if not root:
+    #         return
+    #     if len(res) < level+1:
+    #         res.append([])
+    #     res[level].append(root.val)
+    #     self.dfs(root.left, level+1, res)
+    #     self.dfs(root.right, level+1, res)
+
+
+
+    # DFS Stack
+    # 60ms+
+        res, stack = [], [(root, 0)]
+        while stack:
+            node, level = stack.pop() # pop(0) is 10ms faster
+            if node:
+                if len(res) < level+1:
+                    res.append([])
+                res[level].append(node.val)
+                stack.append((node.right, level+1)) 
+                stack.append((node.left, level+1)) # left LIFO
         return res
 
-    def dfs(self, root, level, res):
-        if not root:
-            return
-        if len(res) < level+1:
-            res.append([])
-        res[level].append(root.val)
-        self.dfs(root.left, level+1, res)
-        self.dfs(root.right, level+1, res)
+
+
+    # https://leetcode.com/discuss/45419/python-short-dfs-solution?show=45422#a45422
