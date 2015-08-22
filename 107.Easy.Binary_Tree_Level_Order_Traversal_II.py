8fac3831061314df7c7705fb51210237a4605bd6
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-22 22:28:22 +0800

    107.Easy.Binary_Tree_Level_Order_Traversal_II.py

--- 107.Easy.Binary_Tree_Level_Order_Traversal_II.py
+++ 107.Easy.Binary_Tree_Level_Order_Traversal_II.py
@@ -13,13 +13,31 @@ class Solution(object):
         """
     # DFS rcs
     # 60ms+
+    #     res = []
+    #     self.dfs(root, 0, res)
+    #     return res
+    # def dfs(self, root, level, res):
+    #     if root:
+    #         if len(res) < level+1:
+    #             res.insert(0,[])
+    #         res[-1-level].append(root.val)
+    #         self.dfs(root.left, level+1, res)
+    #         self.dfs(root.right, level+1, res)
+
+
+
+    # BFS
+    # 60ms+
         res = []
-        self.dfs(root, 0, res)
+        stack = [(root, 0)]
+        while stack:
+            node, level = stack.pop(0)
+            if node:
+                if len(res) < level+1:
+                    res.insert(0, [])
+                res[-1-level].append(node.val)
+                if node.left:
+                    stack.append((node.left, level+1))
+                if node.right:
+                    stack.append((node.right, level+1))
         return res
-    def dfs(self, root, level, res):
-        if root:
-            if len(res) < level+1:
-                res.insert(0,[])
-            res[-1-level].append(root.val)
-            self.dfs(root.left, level+1, res)
-            self.dfs(root.right, level+1, res)
