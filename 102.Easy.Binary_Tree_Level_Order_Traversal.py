4d0e164da4f351ea9f9c8a358e2ee52b1859b204
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-22 10:08:13 +0800

    102.Easy.Binary_Tree_Level_Order_Traversal.py
    rcs

--- 102.Easy.Binary_Tree_Level_Order_Traversal.py
+++ 102.Easy.Binary_Tree_Level_Order_Traversal.py
@@ -11,18 +11,36 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        # 60ms+
+    # BFS
+    # 60ms+
+        # if not root:
+        #     return []
+        # ret = []
+        # level = [root]
+        # while level:
+        #     ret.append([node.val for node in level])
+        #     nextLevel = []
+        #     for node in level:
+        #         if node.left:
+        #             nextLevel.append(node.left)
+        #         if node.right:
+        #             nextLevel.append(node.right)
+        #     level = nextLevel
+        # return ret
+
+
+
+    # DFS Recursively
+    # 50ms+
+        res = []
+        self.dfs(root, 0, res)
+        return res
+
+    def dfs(self, root, level, res):
         if not root:
-            return []
-        ret = []
-        level = [root]
-        while level:
-            ret.append([node.val for node in level])
-            nextLevel = []
-            for node in level:
-                if node.left:
-                    nextLevel.append(node.left)
-                if node.right:
-                    nextLevel.append(node.right)
-            level = nextLevel
-        return ret
+            return
+        if len(res) < level+1:
+            res.append([])
+        res[level].append(root.val)
+        self.dfs(root.left, level+1, res)
+        self.dfs(root.right, level+1, res)
