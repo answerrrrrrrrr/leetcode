d2b3143953d5cc971204717b851e37ae9a6ab6f4
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-24 23:48:32 +0800

    236.Medium.Lowest_Common_Ancestor_of_a_Binary_Tree.py

--- 236.Medium.Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ 236.Medium.Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -13,6 +13,7 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+    # rcs
     # 120ms+
         if not root:
             return None
@@ -20,6 +21,41 @@ class Solution(object):
             return root
         lca = self.lowestCommonAncestor
         llca, rlca = lca(root.left, p, q), lca(root.right, p, q)
-        if (llca, rlca) == (p, q) or (llca, rlca) == (q, p):
+        # if (llca, rlca) == (p, q) or (llca, rlca) == (q, p):
+        if llca and rlca: 
             return root
         return llca or rlca
+
+    # Simplified
+    # https://leetcode.com/discuss/45386/4-lines-c-java-python-ruby
+    # 110ms+
+        if root in (None, p, q):
+            return root
+        lca = self.lowestCommonAncestor
+        llca, rlca = lca(root.left, p, q), lca(root.right, p, q)
+        return root if (llca and rlca) else (llca or rlca)
+
+    # Code simplified but slower
+    # 200ms+
+        # if root in (None, p, q):    return root
+        # l, r = (self.lowestCommonAncestor(kid, p, q)
+        #     for kid in (root.left, root.right))
+        # return root if l and r else l or r
+
+
+
+    # itr
+    # https://leetcode.com/discuss/45603/iterative-solutions-in-python-c
+
+
+
+
+
+
+
+
+
+
+
+
+
