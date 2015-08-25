5d7a24457fac0407164b313f371409d7c4c6081d
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-08-25 14:11:02 +0800

    235.Easy.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
    itr

--- 235.Easy.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ 235.Easy.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -11,12 +11,25 @@ class Solution:
     # @param {TreeNode} q
     # @return {TreeNode}
     def lowestCommonAncestor(self, root, p, q):
-        lca = root
-        
-        while (p.val < lca.val and q.val < lca.val) or (p.val > lca.val and q.val > lca.val):
-            if p.val < lca.val:
-                lca = lca.left
-            elif p.val > lca.val:
-                lca = lca.right
-        
-        return lca
+    # itr
+    # 132ms
+        # lca = root
+        # while (p.val < lca.val and q.val < lca.val) or (p.val > lca.val and q.val > lca.val):
+        #     if p.val < lca.val:
+        #         lca = lca.left
+        #     elif p.val > lca.val:
+        #         lca = lca.right
+        # return lca
+
+# https://leetcode.com/discuss/44959/3-lines-with-o-1-space-1-liners-alternatives        
+    # Simplified
+        while (root.val - p.val)*(root.val - q.val) > 0:
+            # root = (root.left, root.right)[p.val > root.val]
+            root = root.right if p.val > root.val else root.left
+        return root
+
+    # Other way
+        a, b = sorted([p.val, q.val])
+        while not a <= root.val <= b:
+            root = (root.left, root.right)[a > root.val]
+        return root
