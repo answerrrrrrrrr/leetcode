8b1283b1de9328d33eb4c1aec42cd68dbed7fc36
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-09-13 17:48:25 +0800

    24.Medium.SwapNodesInPairs_dummy.py

---
+++ 24.Medium.SwapNodesInPairs_dummy.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+    # rcs
+    # 44ms
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
+
+
+
+    # itr
+    # 44ms
+        dummy = p = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            h = head.next
+            p.next, h.next, head.next = h, head, h.next
+            p, head = head, head.next
+        return dummy.next
