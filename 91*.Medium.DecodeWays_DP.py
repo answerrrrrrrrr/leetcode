d77163e65d7ffb495d0602d113d2ff12f1a91a5f
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-10-12 11:39:45 +0800

    revised

--- 91*.Medium.DecodeWays_DP.py
+++ 91*.Medium.DecodeWays_DP.py
@@ -14,7 +14,7 @@ class Solution(object):
         dp[0] = 1
         for i in range(1, len(s)+1):
             if s[i-1] != "0":
-                s[i] += s[i-1]
+                dp[i] += dp[i-1]
             if i > 1 and "09" < s[i-2:i] < "27":
                 dp[i] += dp[i-2]
         return dp[-1]
