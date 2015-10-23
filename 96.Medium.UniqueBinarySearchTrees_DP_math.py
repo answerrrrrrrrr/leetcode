194efd4c89ff3d2eb13cc0ad3e1db6f1fc961c33
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-10-23 16:54:16 +0800

    96.Medium.UniqueBinarySearchTrees_DP_math.py

---
+++ 96.Medium.UniqueBinarySearchTrees_DP_math.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+    # DP
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in xrange(1, n+1):
+            for j in xrange(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]
+
+
+
+    # Catalan Number  (2n)!/((n+1)!*n!)  
+        return math.factorial(2*n)/(math.factorial(n)*math.factorial(n+1))
