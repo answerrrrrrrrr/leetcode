067bab9564d10976dada6cb18bf1a83b59cb6558
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-10-25 11:04:40 +0800

    96.Medium.UniqueBinarySearchTrees_DP_math.py

--- 96.Medium.UniqueBinarySearchTrees_DP_math.py
+++ 96.Medium.UniqueBinarySearchTrees_DP_math.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+# https://leetcode.com/discuss/47960/python-solutions-dp-catalan-number
+    # https://leetcode.com/discuss/24282/dp-solution-in-6-lines-with-explanation-f-i-n-g-i-1-g-n-i
     # DP
         res = [0] * (n+1)
         res[0] = 1
-        for i in xrange(1, n+1):
-            for j in xrange(i):
+        for i in xrange(1, n+1): # i numbers
+            for j in xrange(i):  # j as root
                 res[i] += res[j] * res[i-1-j]
         return res[n]
 
