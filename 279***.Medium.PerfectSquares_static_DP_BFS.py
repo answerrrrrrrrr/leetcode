ac00c80e722a9bda433d1b8f5130eeffe9ff54c9
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-11-10 11:17:21 +0800

    279***.Medium.PerfectSquares_static_DP_BFS.py
    static

--- 279***.Medium.PerfectSquares_static_DP_BFS.py
+++ 279***.Medium.PerfectSquares_static_DP_BFS.py
@@ -7,7 +7,8 @@ class Solution(object):
 # https://leetcode.com/discuss/58056/summary-of-different-solutions-bfs-static-and-mathematics
 
 
-        
+
+# BFS       
 # https://leetcode.com/discuss/62229/short-python-solution-using-bfs
     # like BFS for the shortest path of a tree
     # 1700ms+
@@ -62,3 +63,29 @@ class Solution(object):
                 if k not in visited:
                     visited.add(k)
                     queue.append((k, step))
+
+# DP
+    # normal dp
+    # TLE
+
+
+# https://leetcode.com/discuss/62313/5-lines-python-static-dp-160ms
+# https://leetcode.com/discuss/56993/static-dp-c-12-ms-python-172-ms-ruby-384-ms
+    # static dp
+    # 164ms
+    _dp = [0]
+    def numSquares(self, n):
+        dp = self._dp
+        while len(dp) <= n:
+            sqs = [i**2 for i in xrange(1, int(n**0.5)+1)]
+            for i in xrange(len(dp), n+1):
+                dp.append(min(1 + dp[i-sq] for sq in sqs if i >= sq))
+        return dp[n]
+
+    # code simlpified
+    _dp = [0]
+    def numSquares(self, n):
+        dp = self._dp
+        while len(dp) <= n:
+            dp += 1 + min(dp[-i*i] for i in xrange(1, int(n**0.5)+1)),
+        return dp[n]
