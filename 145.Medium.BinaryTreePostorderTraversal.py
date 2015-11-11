9f054f12ed49de1b5616531d63400946f71005a9
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2015-11-11 09:13:03 +0800

    145.Medium.BinaryTreePostorderTraversal.py

---
+++ 145.Medium.BinaryTreePostorderTraversal.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+    # insert
+        res, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.insert(0, node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res
+
+
+
+    # reverse
+        res, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
+
+
+
+    # flag
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+        return res
