7bcc4eb3cc1519f2368954dfece13377820cc247
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2016-08-22 15:29:20 +0800

    pep8

--- 5*.Medium.LongestPalindromicSubstring_DP_Manacher.py
+++ 5*.Medium.LongestPalindromicSubstring_DP_Manacher.py
@@ -1,25 +1,32 @@
+# Given a string S, find the longest palindromic substring in S. You may
+# assume that the maximum length of S is 1000, and there exists one unique
+# longest palindromic substring.
+
+
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-    # O(n^2) 
+    # O(n^2)
     # Traverse, grow multiple times
     # 1300ms
         ret = ''
         n = len(s)
         for i in xrange(n):
             for k in range(2):
-                subs = self.getPalindrome(s, i, i+k)
+                subs = self.getPalindrome(s, i, i + k)
                 if len(subs) > len(ret):
                     ret = subs
         return ret
+
     def getPalindrome(self, s, l, r):
         n = len(s)
         while l >= 0 and r < n and s[l] == s[r]:
-            l -= 1; r += 1
-        return s[l +1:r]
+            l -= 1
+            r += 1
+        return s[l + 1:r]
 
     # O(n^2)
     # Traverse, grow once, compare by slicing
@@ -29,14 +36,13 @@ class Solution(object):
         radius = 1
         for i in xrange(n):
             for k in range(2):
-                radius -= 1 # 
-                while i-radius >= 0 and i+k+radius < n and s[i-radius:i +1] == s[i+k:i+k+radius +1][::-1]:
+                radius -= 1
+                while i - radius >= 0 and i + k + radius < n and s[i - radius:i + 1] == s[i + k:i + k + radius + 1][::-1]:
                     radius += 1
-                subs = s[i-radius +1:i+k+radius]
+                subs = s[i - radius + 1:i + k + radius]
                 if len(subs) > len(ret):
                     ret = subs
         return ret
-
 
 
 # https://leetcode.com/discuss/21332/python-o-n-2-method-with-some-optimization-88ms
@@ -46,15 +52,14 @@ class Solution(object):
         start = 0
         maxLen = 1
         for i in xrange(len(s)):
-            if i-maxLen-1 >= 0 and s[i-maxLen-1:i +1] == s[i-maxLen-1:i +1][::-1]:
-                start = i-maxLen-1
+            if i - maxLen - 1 >= 0 and s[i - maxLen - 1:i + 1] == s[i - maxLen - 1:i + 1][::-1]:
+                start = i - maxLen - 1
                 maxLen += 2
                 continue
-            if i-maxLen >= 0 and s[i-maxLen:i +1] == s[i-maxLen:i +1][::-1]:
-                start = i-maxLen
+            if i - maxLen >= 0 and s[i - maxLen:i + 1] == s[i - maxLen:i + 1][::-1]:
+                start = i - maxLen
                 maxLen += 1
-        return s[start:start+maxLen]
-
+        return s[start:start + maxLen]
 
 
 # Manacher Algorithm
@@ -70,43 +75,12 @@ class Solution(object):
         p = [0] * n
         c = r = 0
         maxLen, center = 0, 0
-        for i in xrange(1, n-1):
-            p[i] = (r > i) and min(r-i, p[2*c-i])
-            while t[i-p[i]-1] == t[i+p[i]+1]:
-                p[i] += 1 
-            if r < i+p[i]:
-                c, r = i, i+p[i]
+        for i in xrange(1, n - 1):
+            p[i] = (r > i) and min(r - i, p[2 * c - i])
+            while t[i - p[i] - 1] == t[i + p[i] + 1]:
+                p[i] += 1
+            if r < i + p[i]:
+                c, r = i, i + p[i]
                 if maxLen < p[i]:
                     maxLen, center = p[i], i
-        return s[(center-maxLen)/2:(center+maxLen)/2]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+        return s[(center - maxLen) / 2:(center + maxLen) / 2]
