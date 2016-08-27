dbc23a90972dbaaab8af43c1a14c4c09defcb721
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2016-08-27 12:19:06 +0800

    i,j

--- 27.Easy.RemoveElement.py
+++ 27.Easy.RemoveElement.py
@@ -6,8 +6,16 @@ class Solution:
         # while val in nums:
         #     nums.remove(val)
         # return len(nums)
-        try:
-            while 1:
-                nums.remove(val)
-        except:
-            return len(nums)
+
+        # try:
+        #     while 1:
+        #         nums.remove(val)
+        # except:
+        #     return len(nums)
+
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        return i
